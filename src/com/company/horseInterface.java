7221436adf297b9a8fe5815affac663d6ca1ea14
BT_1N3_04@bktech.local <BT_1N3_04@bktech.local>
2018-11-16 08:42:55 -0500

    Initial commit

---
+++ src/com/company/horseInterface.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface horseInterface {
+    public class mustang implements horse{
+        super (Mustang);
+        String getName;
+        int getweight
+    }
+}
